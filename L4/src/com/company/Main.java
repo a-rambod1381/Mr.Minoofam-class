6a5824f4ecbd2c26e5aeb8ec6c19b17a3d58b478
a-rambod1381 <a.rambod1381@gmail.com>
2022-03-29 15:01:53 +0430

    l4

--- L4/src/com/company/Main.java
+++ L4/src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Person p = new Person();
+        p.setName("Alireza");
+        p.setFamily("Rambod");
+        p.setAge(19);
+        p.setJob("Student");
+        p.setUsername("a-rambod");
+        p.setPass("a.rambod1381");
+        System.out.println("Name: "+p.getName());
+        System.out.println("Family: "+p.getFamily());
+        System.out.println("Age: "+p.getAge());
+        System.out.println("Job: "+p.getJob());
+        System.out.println("Username: "+p.getUsername());
+        System.out.println("Password: "+p.getPass());
     }
 }
---
+++ L4/src/homework3/Student.java
@@ -0,0 +1,31 @@
+package homework3;
+
+public class Student {
+    private String Name;
+    private String Student_Number;
+    private String Father_Name;
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setStudent_Number(String student_Number) {
+        Student_Number = student_Number;
+    }
+
+    public void setFather_Name(String father_Name) {
+        Father_Name = father_Name;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getStudent_Number() {
+        return Student_Number;
+    }
+
+    public String getFather_Name() {
+        return Father_Name;
+    }
+}
