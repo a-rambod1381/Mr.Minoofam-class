36838359c6aabf4b32cd478db80c516930e3f26f
a-rambod1381 <a.rambod1381@gmail.com>
2022-04-09 20:20:32 +0430

    chanege

---
+++ L5/src/classwork/calculate.java
@@ -0,0 +1,11 @@
+package classwork;
+
+public class calculate {
+
+    public class calcPlus{
+        private void calc(){}
+    }
+    public class calcMinus{
+        private void calc(){}
+    }
+}
